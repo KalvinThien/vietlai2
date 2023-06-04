9f55a318539bb570f868ec85fb8f88047dbbc4ed
KalvinThien <120565705+KalvinThien@users.noreply.github.com>
2023-06-04 16:46:31 +0700

    Update rewrite.py

--- rewrite.py
+++ rewrite.py
@@ -6,7 +6,7 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 nltk.download('punkt')
 
 # Set up OpenAI API
-openai.api_key = "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"
+openai.api_key = st.text_input("API Key")
 
 # Function to count tokens (approximate)
 def count_tokens(text):
