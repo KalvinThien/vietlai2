06803492c3491affcabd838c1248ae46e456f48d
KalvinThien <120565705+KalvinThien@users.noreply.github.com>
2023-06-05 14:52:20 +0700

    Update rewrite.py

--- rewrite.py
+++ rewrite.py
@@ -7,7 +7,7 @@ import streamlit as st
 from urllib.parse import urlparse
 
 nltk.download('punkt')
-
+st.title("Rewrite content")
 st.markdown("By [Nguyễn Ngọc Thiện](https://techreviews.vn)")
 st.markdown('---')
 openai.api_key = st.text_input("Điền API Key Chatgpt -   [Lấy API KEY ChatGPT Ở Đây](https://platform.openai.com/account/api-keys) --- Nếu Chưa Có? [Mua Tài Khoản Có Sẵn 120$](https://zalo.me/0888884749)", type='password')
