22d1900da75f0bc70bd9d74c0566d3cb89842de8
KalvinThien <120565705+KalvinThien@users.noreply.github.com>
2023-06-06 15:24:36 +0700

    Update rewrite.py

--- rewrite.py
+++ rewrite.py
@@ -7,10 +7,10 @@ import streamlit as st
 from urllib.parse import urlparse
 
 nltk.download('punkt')
-st.title("Rewrite Contents")
+st.title("Viết Lại Nội Dung")
 st.markdown("By [Nguyễn Ngọc Thiện](https://techreviews.vn)")
 st.markdown('---')
-openai.api_key = st.text_input("Điền API Key Chatgpt -   [Lấy API KEY ChatGPT Ở Đây](https://platform.openai.com/account/api-keys) --- Nếu Chưa Có? [Mua Tài Khoản Có Sẵn 120$](https://zalo.me/0888884749)", type='password')
+openai.api_key = st.text_input("Điền API Key Chatgpt - [Lấy API KEY ChatGPT Ở Đây](https://platform.openai.com/account/api-keys) --- Nếu Chưa Có? [Mua Tài Khoản Có Sẵn 120$](https://zalo.me/0888884749)", type='password')
 MODEL = "gpt-3.5-turbo"
 
 def count_tokens(text):
@@ -33,138 +33,79 @@ def split_content(content, max_tokens=1000):
         chunks.append(chunk)
     return chunks
 
-def insert_keywords_links(chunk, main_keyword, secondary_keywords, internal_url):
-    secondary_keywords = secondary_keywords.split(',')
-    urls = [url.strip() for url in internal_url.split(',') if url.strip() != ''] # Added condition to ignore empty URLs
-    url_keywords = []
-
-    for url in urls:
-        path = urlparse(url).path
-        path_keywords = path.strip('/').replace('-', ' ')
-        url_keywords.append((path_keywords, url))
-
-    for keyword, url in url_keywords:
-        if keyword in chunk and chunk.count(url) < 1:
-            chunk = chunk.replace(keyword, f"<a href='{url}'>{keyword}</a>", 1)
-            
-    for keyword in secondary_keywords:
-        if keyword in chunk and urls and chunk.count(urls[0]) < 1:
-            chunk = chunk.replace(keyword, f"<a href='{urls[0]}'>{keyword}</a>", 1)
-
-
-    return chunk
-def title_with_number(title, main_keyword):
-    number_list = list(range(1,100))
-    random.shuffle(number_list)
-    number = number_list[0]
-    new_title = f"{number} {title} {main_keyword}" # Added main_keyword
-    if len(new_title) > 60:
-        new_title = new_title[:60]
-    return new_title
-
-
-styles = ['Creative', 'Formal', 'Informal', 'Academic', 'Conversational', 'Persuasive', 'Descriptive', 'Instructional', 'Amazon Review', 'Amazon Guide']
-selected_style = st.selectbox('Chọn Kiểu Viết', styles)
-selected_style = selected_style.lower().replace(" ", "_")
-
 def generate_rewritten_chunks(content, main_keywords, secondary_keywords, internal_url, style):
     secondary_keywords = secondary_keywords.split(',')
     random.shuffle(secondary_keywords) # shuffle secondary keywords for randomness
 
     chunks = split_content(content)
     rewritten_chunks = []
+
+    # Tạo H1 title và SEO Meta Description
+    h1_title = f"# {main_keywords}" # H1 title with main keyword
+    seo_description = f"SEO Meta Description: {main_keywords}" # SEO Meta Description with main keyword
+    rewritten_chunks.append({h1_title: "", seo_description: ""}) # Add H1 title and SEO Meta Description to the content
+
     for i, chunk in enumerate(chunks):
         secondary_keyword = secondary_keywords[i % len(secondary_keywords)] # cycle through secondary keywords
-        section_title = f"Section {i+1}: {secondary_keyword}"
-        section_title = title_with_number(section_title, secondary_keyword)
-        
-        if style == "creative":
-            system_message = "You are an imaginative and creative writer."
-        elif style == "formal":
-            system_message = "You are a formal writer, providing structured and professional content."
-        elif style == "informal":
-            system_message = "You are an informal writer, providing relaxed and conversational content."
-        elif style == "academic":
-            system_message = "You are an academic writer, focusing on providing informative and scholarly content."
-        elif style == "conversational":
-            system_message = "You are a conversational writer, able to create engaging and interactive content."
-        elif style == "persuasive":
-            system_message = "You are a persuasive writer, skilled at convincing and influencing your readers."
-        elif style == "descriptive":
-            system_message = "You are a descriptive writer, able to paint vivid pictures with your words."
-        elif style == "instructional":
-            system_message = "You are an instructional writer, adept at providing clear and detailed guides."
-        elif style == "amazon_review":
-            system_message = "You are a seasoned reviewer who has written many Amazon product reviews."
-        elif style == "amazon_guide":
-            system_message = "You are an expert on Amazon, providing detailed and helpful guides on how to use its services."
+        section_title = f"## Section {i+1}: {secondary_keyword if i > 0 else main_keywords}" # ensure main keyword only appears in the first H2
 
-        messages = [{'role': 'system', 'content': system_message},
-                    {'role': 'user', 'content': f"Your task is to rewrite the entire text in better language and Rewrite The Paragraph In Your Own Words Rather Than Copying And Pasting From Other Sources. Consider perplexity and burstiness when creating content, ensures high levels of both without losing specificity or context. Use fully detailed paragraphs that engage the reader. Write In A Conversational Style As Written By A Human (Use An Informal Tone, Utilize Personal Pronouns, Keep It Simple , Engage The Reader, Use The Active Voice, Keep It Brief, Use Rhetorical Questions, and Incorporate Analogies And Metaphors). Use the primary keyword '{main_keywords}' under the H2 heading and the secondary keyword '{secondary_keywords}' as content for better writing. Write in Markdown format and try to stretch the content more than the original. this is important to Bold the Title and all headings of the article, and use appropriate headings for H tags.All output should be in {selected_language}:\n\n{chunk}"}]
-
-
+        messages = [{'role': 'system', 'content': f"You are a {style} Your task is to rewrite the entire text in better language and Rewrite The Paragraph In Your Own Words Rather Than Copying And Pasting From Other Sources. Consider perplexity and burstiness when creating content, ensures high levels of both without losing specificity or context. Use fully detailed paragraphs that engage the reader. Write In A Conversational Style As Written By A Human (Use An Informal Tone, Utilize Personal Pronouns, Keep It Simple , Engage The Reader, Use The Active Voice, Keep It Brief, Use Rhetorical Questions, and Incorporate Analogies And Metaphors), incorporating the primary keyword '{main_keywords}' and the secondary keyword '{secondary_keyword}'. Write in a conversational style, demonstrate the content is easy, uses personal pronouns, is to read, brief and uses rhetorical questions. Ensure that the content is written in the Markdown format and try to make it longer than the original. Make sure to bold the title and all headings of the article, and use appropriate headings for H tags. The text to be rewritten is:\n\n{chunk}"}]
         response = openai.ChatCompletion.create(model=MODEL, messages=messages, temperature=0.8, max_tokens=2048)
         output = response.choices[0].message["content"]
+        
+        # Tạo tiêu đề dựa trên nội dung viết lại
+        title_prompt = {'role': 'system', 'content': f"You are a {style}. Your task is to generate H2 and H3 titles based on the following content.Written By A Human (Use An Informal Tone, Utilize Personal Pronouns, Keep It Simple , Engage The Reader, Use The Active Voice, Keep It Brief, Use Rhetorical Questions, and Incorporate Analogies And Metaphors). Write in a conversational style, demonstrate the content is easy, uses personal pronouns, is to read, brief and uses rhetorical questions. Ensure that the content is written in the Markdown :\n\n{output}"}
+        title_response = openai.ChatCompletion.create(model=MODEL, messages=[title_prompt], temperature=0.8, max_tokens=200)
+        section_title = title_response.choices[0].message["content"]
 
-        # Comment out or remove the line below if you only want URLs at the end
-        # output = insert_keywords_links(output, main_keywords, secondary_keywords, internal_url)
-        if i == 0: # only replace the main keyword in the first chunk
-            output = output.replace(main_keywords, f"{main_keywords}", 1)
         rewritten_chunks.append({section_title: output})
     return rewritten_chunks
+def generate_title_and_seo(main_keywords, style):
+    # Tạo prompt
+    messages = [{'role': 'system', 'content': f"You are a {style}. Your task is to generate a H1 title and SEO Meta Description using the main keyword '{main_keywords}'. The title should be informative and engaging. The SEO Meta Description should describe the overall content of the article in a concise and appealing way."}]
+
+    # Gọi ChatGPT
+    response = openai.ChatCompletion.create(model=MODEL, messages=messages, temperature=0.8, max_tokens=100)
+
+    # Lấy kết quả từ phản hồi của ChatGPT
+    output = response.choices[0].message["content"].split('\n')
+
+    # Tạo H1 title và SEO Meta Description
+    h1_title = f"# {output[0]}\n" # H1 title
+    seo_description = f"{output[1]}\n" # SEO Meta Description
+
+    return h1_title, seo_description
+
+
 
 def rewrite_content(content, main_keywords, secondary_keywords, internal_url, style):
     rewritten_chunks = generate_rewritten_chunks(content, main_keywords, secondary_keywords, internal_url, style)
-    rewritten_content = " ".join([chunk[list(chunk.keys())[0]] for chunk in rewritten_chunks])
 
-    # Generate SEO Meta Description
-    seo_description = f"{rewritten_content[:155]}..." # Summary of the content, limited to around 160 characters
+    # Gọi hàm để tạo H1 title và SEO Meta Description
+    h1_title, seo_description = generate_title_and_seo(main_keywords, style)
 
-    # Add SEO Meta Description at the beginning
-    rewritten_content = f"## {seo_description}\n\n{rewritten_content}"
+    # Chuỗi bắt đầu với H1 title và SEO Meta Description
+    rewritten_content = h1_title + seo_description
 
-    # Add internal URLs at the end of the content if they exist
-    urls = [url.strip() for url in internal_url.split(',') if url.strip() != ''] # Added condition to ignore empty URLs
-    if urls:
-        rewritten_content += "\n\n"
-        for url in urls:
-            rewritten_content += f"<a href='{url}'>{urlparse(url).path.strip('/').replace('-', ' ')}</a>\n"
-    
+    # Thêm các phần viết lại vào nội dung
+    for chunk in rewritten_chunks:
+        section_title = list(chunk.keys())[0]  
+        section_content = chunk[section_title] + "\n"
+        rewritten_content += section_title + "\n" + section_content
+
     return rewritten_content
 
-def generate_faq(content):
-    system_message = "You are an intelligent AI that can generate insightful Frequently Asked Questions and their corresponding answers based on the provided content "
-    user_message = f"Generate 5 pairs of Frequently Asked Questions and their Answers based on the following content: \n\n{content} "
-
-    messages = [{'role': 'system', 'content': system_message}, {'role': 'user', 'content': user_message}]
-    response = openai.ChatCompletion.create(model=MODEL, messages=messages, temperature=0.8, max_tokens=1000) # Increase max_tokens to have room for answers
-
-    faq_pairs = response.choices[0].message["content"].split('\n\n') # Split by two newline characters to separate questions and answers
-    faq_list = []
-    for faq_pair in faq_pairs:
-        if faq_pair.strip() != '':
-            faq_list.append(faq_pair.strip().split('\n')) # Split by one newline character to separate a question from its answer
-    return faq_list[:5]  # Limit to
 
 
 content = st.text_area('Điền Nội Dung Cần Viết Lại', '')
 languages = ['Vietnamese', 'English', 'Lao', 'Thai', 'Filipino']
 selected_language = st.selectbox('Chọn Ngôn Ngữ Viết Lại', languages)
+styles = ['Creative', 'Formal', 'Informal', 'Academic', 'Conversational', 'Persuasive', 'Descriptive', 'Instructional', 'Amazon Review', 'Amazon Guide']
+selected_style = st.selectbox('Chọn Kiểu Viết', styles)
 main_keywords = st.text_input('Điền Từ Khoá Chính', '')
-secondary_keywords = st.text_input('Từ khoá phụ , cách nhau dấu phẩy', '')
-internal_url = st.text_input('Điền Link Internal, cách nhau bởi dấu phẩy ', '')
-faq_option = st.checkbox('Tạo 5 câu hỏi thường gặp hay không?')
+secondary_keywords = st.text_input('Từ khoá phụ, cách nhau bằng dấu phẩy', '')
+internal_url = st.text_input('Điền Link Internal, cách nhau bởi dấu phẩy', '')
 
 if st.button('Viết Lại'):
-    rewritten_content = rewrite_content(content, main_keywords, secondary_keywords, internal_url, selected_style)
-
-    # Generate FAQs if the option is selected
-    if faq_option:
-        faqs = generate_faq(rewritten_content)
-        faq_section = "\n## FAQ \n"
-        for i, faq_pair in enumerate(faqs):
-            question, answer = faq_pair[0], faq_pair[1] if len(faq_pair) > 1 else 'loading....'
-            faq_section += f"\n### {i+1}: {question}\n\n: {answer}\n"
-        rewritten_content += faq_section
-
-    st.write(rewritten_content)
+    rewritten_content = rewrite_content(content, main_keywords, secondary_keywords, internal_url, selected_style.lower())
+    st.markdown(rewritten_content)
